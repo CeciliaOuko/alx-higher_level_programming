811bd4f8a83017114384c17865cbcb291975c14c
CeciliaOuko <cecilia.ouko@gmail.com>
2022-12-04 12:00:57 -0800

    13-is_palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -23,18 +23,18 @@ int is_palindrome(listint_t **head);
 
 listint_t *reverse_listint(listint_t **head)
 {
-		listint_t *node = *head, *next, *prev = NULL;
+	listint_t *node = *head, *next, *prev = NULL;
 
-		while (node)
-		{
-			next = node->next;
-			node->next = prev;
-			prev = node;
-			node = next;
-		}
+	while (node)
+	{
+		next = node->next;
+		node->next = prev;
+		prev = node;
+		node = next;
+	}
 
-			*head = prev;
-			return (*head);
+		*head = prev;
+		return (*head);
 }
 
 /**
